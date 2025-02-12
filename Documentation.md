cb7ff2b66dc4ac007341c6bdef6dfceda514ba29
thomas <thomas.baullard@gmail.com>
2025-02-12 15:21:58 +0100

    création de l'architecture et configuration de la db

---
+++ Documentation.md
@@ -0,0 +1,10 @@
+##Initialisation d'un projet Node
+`
+npm init -y
+`
+
+##Insatllation des dépendances
+`
+ npm i nodemon express mongoose dotenv bcrypt jsonwebtoken helmet cloudinary multer cors
+`
+
---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Base de données connetée`)
+    } catch (error) {
+        console.error(`Erreur lors de la connexion à la base de donnée`, error);
+    }
+}
+
+module.exports = connectDB;
