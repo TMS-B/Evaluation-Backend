95db020b0d6b07b96211a1fdd8b5b212557c84ab
thomas <thomas.baullard@gmail.com>
2025-02-20 22:55:40 +0100

    finalisation SkillsControllers

---
+++ backend/controllers/skillControllers.js
@@ -0,0 +1,87 @@
+const Skill = require("../models/skillsModel");
+const User = require("../models/User");
+const { v2: cloudinary } = require("cloudinary");
+const fs = require("fs");
+
+exports.createSkill = async (req, res, next) => {
+  const { titre, catégorie, niveau } = req.body;
+  const userId = req.user_id;
+  try {
+    if (!req.file) {
+      return next({
+        status: 400,
+        message: `Une image est nécéssaire à la création du compte`,
+      });
+    }
+    const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+      folder: "skills",
+    });
+    fs.unlinkSync(req.file.path);
+    const skill = new Skill({
+      titre,
+      catégorie,
+      niveau,
+      public_id: uploadResult.public_id,
+      url: uploadResult.secure_url,
+    });
+    await skill.save();
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $addToSet: { skill: newSkill } },
+      { new: true }
+    ).select("-password");
+    await user.save();
+
+    res.status(201).json({ message: `Compétences créer avec succès`, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getAllAsset = async (req, res) => {
+  try {
+    const image = await Skill.find();
+    res.json({ image });
+  } catch (error) {
+    console.error(`Impossible de récupérer le média`);
+    res.status(500).json({ error: `Something went wrong` });
+  }
+};
+
+exports.updateAsset = async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  try {
+    const image = await Skill.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
+    if (!image) {
+      return res
+        .status(500)
+        .json({ message: `Erreur lors de la modification de l'image` });
+    }
+    res.json(image);
+  } catch (error) {
+    console.error(`Erreur de l'envoi coté serveur`, error);
+  }
+};
+
+exports.deleteAsset = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const skill = await Skill.findByIdAndDelete(id);
+    if (!skill) {
+      return res.status(404).json({ error: `Compétences introuvable` });
+    }
+    await cloudinary.uploader.destroy(skill.public_id);
+    res.json({ message: `Le fichier a bien été supprimé` });
+  } catch (error) {
+    console.error(`Erreur lors de la suppression`, error);
+    res
+      .status(500)
+      .json({ error: `Impossible de supprimer le fichier coté serveur` });
+  }
+};
--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password:  { type: String, required: true },
+    password:  { type: String, required: true, select: false },
     role: { type: String, enum: [ "user", "admin" ], default: "user" }
     },
     {
