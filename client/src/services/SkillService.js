93cbf1e2af03e39f1d2f1042842eec15090578e2
Your Name <you@example.com>
2025-03-01 19:43:25 +0100

    changed api_url

--- client/src/services/SkillService.js
+++ client/src/services/SkillService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-    baseURL: "http://localhost:3101/api/skills",
+    baseURL: `${API_URL}/api/skills`,
 });
 
 export const getAllSkills = async () => {
